23738187aefe6779f0c739de674bab594c0f9764
Phát <nguyenphatit.nl@gmail.com>
2018-05-25 21:29:22 +0700

    style
    style

--- src/app/views/home/phan-cong-day/phan-cong-day.module.ts
+++ src/app/views/home/phan-cong-day/phan-cong-day.module.ts
@@ -4,7 +4,6 @@ import { PhanCongDayRoutingModule } from './phan-cong-day-routing.module';
 import { MonHocPhanCongComponent } from './mon-hoc-phan-cong/mon-hoc-phan-cong.component';
 import { ChiTietMonHocComponent } from './chi-tiet-mon-hoc/chi-tiet-mon-hoc.component';
 import { BoMonPhanCongComponent } from './bo-mon-phan-cong/bo-mon-phan-cong.component';
-import { DanhSachMonHocComponent } from '../mon-hoc/danh-sach-mon-hoc/danh-sach-mon-hoc.component';
 import { MaterialModule } from '../../../material.module';
 
 
@@ -18,7 +17,6 @@ import { MaterialModule } from '../../../material.module';
   declarations: [
     MonHocPhanCongComponent,
     BoMonPhanCongComponent,
-    DanhSachMonHocComponent,
     ChiTietMonHocComponent
   ],
   providers: [],
