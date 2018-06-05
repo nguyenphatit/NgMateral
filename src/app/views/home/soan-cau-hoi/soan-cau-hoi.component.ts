e1653df5100af669b0737783c87b799850b2f9a1
vanthang1996 <vanthang20437@gmail.com>
2018-06-06 00:30:29 +0700

    Thang123

--- src/app/views/home/soan-cau-hoi/soan-cau-hoi.component.ts
+++ src/app/views/home/soan-cau-hoi/soan-cau-hoi.component.ts
@@ -1,3 +1,6 @@
+import { NotifyCenterService } from './../../../_services/notify-center.service';
+import { Question } from './../../../_models/question';
+import { Answer } from './../../../_models/answer';
 import { QuestionService } from './../../../_services/questionService.service';
 import { JobService } from './../../../_services/job.service';
 import { Component, OnInit } from '@angular/core';
@@ -12,11 +15,13 @@ import { map } from 'rxjs/operators';
 export class SoanCauHoiComponent implements OnInit {
   isTL = true;
   isTN = true;
-  numAnswer: Number;
+  numAnswer: Array<Answer>;
   subjects;
   selectedSubject;
   questions;
   subjectId = 0;
+  question = new Question();
+  contentAnswerTl;
   //
   numberOfRecord: number;
   numberOfPage: number;
@@ -24,7 +29,7 @@ export class SoanCauHoiComponent implements OnInit {
   collectionPageShow;
   limit = 3;
   page_show = true;
-  number_showPape = 3; // số button tối đa hiển thị, chọn số lẻ
+  number_showPape = 5; // số button tối đa hiển thị, chọn số lẻ
   movePage(page: number): void {
     this.page_show = false;
     this.current_page = page;
@@ -42,14 +47,14 @@ export class SoanCauHoiComponent implements OnInit {
   fillRange(start: number, end: number): Array<number> {
     return Array(end - start + 1).fill(0).map((item, index) => start + index);
   }
-  collectionPage(current_page: number, max_numberOfPage): Array<number> {
+  collectionPage(current_page: number, max_numberOfPage: number, number_showPape: number): Array<number> {
     let arrays: any;
-    const start = Math.ceil(this.number_showPape / 2);
+    const start = Math.ceil(number_showPape / 2);
     const end = max_numberOfPage - (start - 1);
     if (current_page < start) {
-      arrays = this.fillRange(1, this.number_showPape);
+      arrays = this.fillRange(1, number_showPape);
     } else if (current_page > end) {
-      arrays = this.fillRange(max_numberOfPage - this.number_showPape + 1, max_numberOfPage);
+      arrays = this.fillRange(max_numberOfPage - number_showPape + 1, max_numberOfPage);
     } else {
       arrays = this.fillRange(current_page - (start - 1), current_page + (start - 1));
     } console.log(arrays);
@@ -69,7 +74,8 @@ export class SoanCauHoiComponent implements OnInit {
 
 
   constructor(private teacherService: TeacherService
-    , private questionService: QuestionService) { }
+    , private questionService: QuestionService
+    , private notifyCenterService: NotifyCenterService) { }
 
   ngOnInit() {
     this.teacherService.getSubjectOfTeacher().subscribe((data: any) => {
@@ -81,29 +87,62 @@ export class SoanCauHoiComponent implements OnInit {
   openTL(): void {
     this.isTL = false;
     this.isTN = true;
+    this.numberQuestion(1);
   }
 
   openTN(): void {
     this.isTL = true;
     this.isTN = false;
+    this.numberQuestion(0);
   }
   numberQuestion(number: number) {
-    this.numAnswer = number;
-    console.log(number);
+    this.numAnswer = Array(+number).fill(0).map((item, index) => new Answer());
   }
   changeSubject(subjectId: number) {
     this.subjectId = +subjectId;
+    this.selectedSubject = this.subjects.find(subject => subject.subjectId === this.subjectId);
+    console.log(this.selectedSubject);
     this.loadData();
   }
 
   loadData() {
+    this.question.chapterId = 0;
+    this.question.levelId = 1;
     this.questionService.getQuestionOfTeacherCompile(this.subjectId, this.current_page, this.limit)
       .subscribe((data: any) => {
         this.questions = data.listOfResult;
         this.numberOfPage = data.numberOfPage;
         this.numberOfRecord = data.numberOfRecord;
-        this.collectionPageShow = this.collectionPage(this.current_page, this.numberOfPage);
+        this.collectionPageShow = this.collectionPage(this.current_page, this.numberOfPage, this.number_showPape);
         console.log(data);
       });
+  }
+  addQuestion() {
+    if (!this.isTL) {
+      this.numAnswer[0].correctAnswer = true;
+      this.numAnswer[0].content = this.contentAnswerTl;
+    }
+    if (!this.hasOneAnswerCorrect(this.numAnswer)) {
+      this.notifyCenterService.sendNotifyCenter({ massage: 'Phải có ít nhất một đáp án đúng!', status: null, details: null });
+      return;
+    }
+    this.question.answers = this.numAnswer;
+    this.questionService.insertQuestion(this.question).subscribe((data: any) => {
+      console.log('Dữ liệu:', data);
+    });
+  }
+  clearModal() {
+    this.numberQuestion(0);
+    this.contentAnswerTl = '';
+    this.isTL = true;
+    this.isTN = true;
+    this.question = new Question();
+    this.question.chapterId = 0;
+    this.question.levelId = 1;
+  }
+  hasOneAnswerCorrect(answers: Array<Answer>): boolean {
+    answers.forEach((answer) => {
+      if (answer.correctAnswer) { return true; }
+    }); return false;
   }
 }
